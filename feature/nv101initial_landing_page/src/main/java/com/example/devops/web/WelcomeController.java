a5bc8976bfdb8db823f006578a22c427f33996ac
Alexis <alex-zero@live.com>
2018-08-30 15:34:59 -0500

    update front end  and repo
    new features on the front end using bootstrap and modifications for integrate artifactory and maven

--- feature/nv101initial_landing_page/src/main/java/com/example/devops/web/WelcomeController.java
+++ feature/nv101initial_landing_page/src/main/java/com/example/devops/web/WelcomeController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WelcomeController {
-    
-    @RequestMapping("/")
-    public String welcome(Model model){
-        model.addAttribute("course","DevOps");
-        return "index";
-    }
-    
+
+	@RequestMapping("/")
+	public String welcome(Model model) {
+		model.addAttribute("course", "DevOps");
+		return "index";
+	}
+
 }
